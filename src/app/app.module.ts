614458d7fad1c14c2c59bd15a496b417e6b82990
khalil <khalillakhdharatc@gmail.com>
2023-06-05 11:24:03 +0100

    cons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { TachesComponent } from './taches/taches.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroductionComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    TachesComponent
   ],
   imports: [
     BrowserModule
