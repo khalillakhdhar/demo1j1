7f57f3d2cef4d3018047a39144233063a69b03d3
khalil <khalillakhdharatc@gmail.com>
2023-06-05 12:44:13 +0100

    update

--- src/app/taches/taches.component.ts
+++ src/app/taches/taches.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./taches.component.css']
 })
 export class TachesComponent {
-taches=["t1", "t2", "t3", "t4"];
-visible=false;
+taches=["t1", "t2", "t3", "t4"]; // déclarations d'un tableaux des taches
+visible=false; // initialisation de la variable de visibilité
+visibility()
+{
+  this.visible= !this.visible;
+}
 }
